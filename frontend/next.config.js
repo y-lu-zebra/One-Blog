f697f4636817ec304187e4a5bcf867c964db9e7e
盧 宇 <y.lu@zebra-company.jp>
2023-12-22 11:40:21 +0900

    Refs #5 【フロントエンド】.env ファイルを追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+// 環境設定ファイル変更
+require('dotenv').config({ path: '../.env' })
+
+const nextConfig = {
+  env: {
+    // アプリ名称
+    APP_NAME: process.env.APP_NAME,
+    // UI の URL
+    APP_URL: process.env.APP_URL,
+    // API の URL
+    API_URL: process.env.API_URL,
+  },
+}
 
 module.exports = nextConfig
