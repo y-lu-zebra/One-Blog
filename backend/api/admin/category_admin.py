54b2f9da4e021646c61b9ac185dcc4894dff4ee9
盧 宇 <y.lu@zebra-company.jp>
2023-12-27 13:32:57 +0900

    Refs #13 カテゴリー ADMIN の表示設定を変更
    - 新規作成画面の表示項目のグループ化

--- backend/api/admin/category_admin.py
+++ backend/api/admin/category_admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
 
 from api.models import Categories
 
 
+@admin.register(Categories)
 class CategoryAdmin(admin.ModelAdmin):
     """
     カテゴリー ADMIN
     """
 
+    # 一覧画面に表示するフィールド
     list_display = [
         # カテゴリー名
         "name",
@@ -16,12 +19,55 @@ class CategoryAdmin(admin.ModelAdmin):
         # カテゴリー別名
         "alias",
         # 最終更新日時
-        "updated_at",
+        "date_updated",
     ]
+    # 一覧画面にリンクで表示するフィールド
     list_display_links = [
         # カテゴリー名
         "name",
     ]
-
-
-admin.site.register(Categories, CategoryAdmin)
+    # 新規作成画面に表示するフィールド構成
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    # カテゴリー名
+                    "name",
+                    # タイプ
+                    "type",
+                    # 親カテゴリー
+                    "parent",
+                    # 並び順
+                    "sort_order",
+                    # 作成者
+                    "user_created",
+                    # 最終更新者
+                    "user_updated",
+                ],
+            },
+        ),
+        # オプション
+        (
+            _("Advanced options"),
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "alias",
+                    "url",
+                ],
+            },
+        ),
+        # SEO
+        (
+            _("SEO Information"),
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "meta_title",
+                    "meta_description",
+                    "meta_keywords",
+                ],
+            },
+        ),
+    ]
