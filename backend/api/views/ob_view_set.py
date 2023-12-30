705a32c278f2e7917d38088558723b8af004b278
盧 宇 <y.lu@zebra-company.jp>
2023-12-30 23:40:06 +0900

    Refs #16 API の作成者と更新者に自動追加処理を追加

--- backend/api/views/ob_view_set.py
+++ backend/api/views/ob_view_set.py
@@ -10,3 +10,33 @@ class OBViewSet(viewsets.ModelViewSet):
 
     # ロガー
     logger = logging.getLogger("django")
+
+    def perform_create(self, serializer):
+        """
+        新規作成 API が呼び出された際に、作成者と最終更新者を自動追加する。
+
+        Parameters
+        ----------
+        serializer
+            シリアライザー
+        Returns
+        -------
+            なし
+        """
+
+        serializer.save(user_created=self.request.user, user_updated=self.request.user)
+
+    def perform_update(self, serializer):
+        """
+        更新 API が呼び出された際に、最終更新者を自動更新する。
+
+        Parameters
+        ----------
+        serializer
+            シリアライザー
+        Returns
+        -------
+            なし
+        """
+
+        serializer.save(user_updated=self.request.user)
