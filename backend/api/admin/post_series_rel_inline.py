5cab905675f8e97e07d608676ef52180b0004e66
盧 宇 <y.lu@zebra-company.jp>
2024-01-05 12:36:18 +0900

    Refs #23 ADMIN インラインクラスの docstring を追記

--- backend/api/admin/post_series_rel_inline.py
+++ backend/api/admin/post_series_rel_inline.py
@@ -4,5 +4,9 @@ from api.models.rels import PostSeriesRel
 
 
 class PostSeriesRelInline(admin.TabularInline):
+    """
+    「投稿・シリーズ」リレーション（中間）インラインクラス
+    """
+
     model = PostSeriesRel
     extra = 1
