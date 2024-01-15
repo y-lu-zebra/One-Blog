74dfa546327fdf69698a232e745573bd352b80e0
盧 宇 <y.lu@zebra-company.jp>
2024-01-15 17:04:35 +0900

    Refs #25 カテゴリー・データタイプを作成

--- frontend/src/types/category.d.ts
+++ frontend/src/types/category.d.ts
@@ -1,7 +1,25 @@
+import { CategoryType } from '@/types/categoryType'
+
 /**
  * カテゴリーインターフェイス
  */
 interface Category {
+  // カテゴリー ID
   id: number
+  // カテゴリー名
   name: string
+  // タイプ
+  type: CategoryType
+  // カテゴリー別名
+  alias: string
+  // URL
+  url: string
+  // メタタイトル
+  metaTitle: string
+  // メタディスクリプション
+  metaDescription: string
+  // メタキーワード
+  metaKeywords: string
+  // 子カテゴリー
+  children: Category[]
 }
