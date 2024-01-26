c94e7ed22dda09c3df61e5a171111d2e5483af16
盧 宇 <y.lu@zebra-company.jp>
2024-01-26 16:22:59 +0900

    Refs #27 バックエンドの favicon をカスタマイズ

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import RedirectView
 
 from backend import settings
 
@@ -23,7 +22,5 @@ urlpatterns = [
     path(f"{settings.APP_URL_ADMIN}/", admin.site.urls),
     # 認証
     path("api-auth/", include("rest_framework.urls")),
-    # バックエンドの favicon
-    path("favicon.ico", RedirectView.as_view(url="static/favicon.ico")),
 ]
 urlpatterns += staticfiles_urlpatterns()
