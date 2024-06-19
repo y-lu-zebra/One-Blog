415b553fc428c18d9fdb0fd28f44e61dc2ed7414
盧 宇 <y.lu@zebra-company.jp>
2024-06-19 18:26:28 +0900

    feat: #55 Google Analytics コンポーネント作成

--- frontend/src/components/ga.tsx
+++ frontend/src/components/ga.tsx
@@ -4,13 +4,17 @@ import { usePathname, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import ReactGA from 'react-ga4'
 
-export default function GoogleAnalytics() {
+/**
+ * Google Analytics (GA4) コンポーネント
+ * @constructor
+ */
+const GoogleAnalytics = () => {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
     if (process.env.GA_TRACKING_ID) {
-      const url = pathname + searchParams.toString()
+      const url: string = pathname + searchParams.toString()
       ReactGA.initialize(process.env.GA_TRACKING_ID)
       ReactGA.send({ hitType: 'pageview', page: url })
     }
@@ -18,3 +22,5 @@ export default function GoogleAnalytics() {
 
   return <></>
 }
+
+export default GoogleAnalytics
