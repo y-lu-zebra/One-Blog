4f223a312b660334b28e99c296494de6ec336b32
盧 宇 <y.lu@zebra-company.jp>
2024-06-23 00:38:46 +0900

    feat: #55 投稿タイトルを調整

--- frontend/src/__tests__/app/posts/[slug]/page.test.tsx
+++ frontend/src/__tests__/app/posts/[slug]/page.test.tsx
@@ -56,7 +56,7 @@ describe('Get Post Page Generate Meta Data', () => {
       className: 'python',
     })
     expect(metaData).toEqual({
-      title: 'meta title',
+      title: 'meta title | undefined',
       description: 'meta description',
       keywords: 'meta keywords',
     })
@@ -69,7 +69,7 @@ describe('Get Post Page Generate Meta Data', () => {
       className: 'python',
     })
     expect(metaData).toEqual({
-      title: 'test',
+      title: 'test | undefined',
       description: 'meta description',
       keywords: 'meta keywords',
     })
